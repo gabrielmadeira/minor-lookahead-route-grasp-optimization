5ffce2a4a12e736f2d454ef424e43c9e6fe00d08
Gabriel Madeira <gabr.madeira@gmail.com>
2022-09-26 16:47:50 -0300

    starting to formulate the problem for the solver

---
+++ mlr-grasp.jl
@@ -0,0 +1,7 @@
+# Todas implementações devem aceitar uma instância no formato do problema na entrada padrão (stdin) e imprimir a melhor solução encontrada na saída padrão (stdout).
+
+# Os principais parâmetros do método devem ser definíveis pela linha de comando.
+# O primeiro parâmetro da linha de comando é o nome de um arquivo para gravar a melhor solução encontrada.
+
+# Log
+# arquivo final
